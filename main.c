4e238ab86571ff7420e678f032c166f7cdc57bc8
szhou49 <szhou49@illinois.edu>
2024-04-15 17:14:08 -0700

    Update

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "structure.h"
+#include "structure.c"
 
 int main() {
     MinHeap* frequencyTable = countFrequency("Original_text.txt");
     print(frequencyTable);
-    getchar();
     return 0;
 }
