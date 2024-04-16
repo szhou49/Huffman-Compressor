83825047b9594a8d7464fa64060f58f90511de60
XIYANG1018 <pkuyangxi@163.com>
2024-04-16 14:39:01 -0700

    add encode function

--- main.c
+++ main.c
@@ -6,14 +6,19 @@
 MinHeap* countFrequency(char* filename);
 TreeNode* buildHuffmanTree(MinHeap* minHeap);
 void printHuffmanTree(TreeNode* root);
+void encode(TreeNode* root);
 
 int main() {
 
     MinHeap* frequencyTable = countFrequency("Original_text.txt");
 
+
     TreeNode* huffmanTree = buildHuffmanTree(frequencyTable);
 
     printHuffmanTree(huffmanTree);
+
+
+    encode(huffmanTree);
 
 
     return 0;
--- tree_building.c
+++ tree_building.c
@@ -17,6 +17,7 @@ TreeNode* buildHuffmanTree(MinHeap* minHeap) {
     return pop(minHeap);
 }
 
+// Helper function to print the huffman tree
 void printHuffmanTree(TreeNode* root) {
     if (root != NULL) {
         printf("%c : %d\n", root->val, root->freq);
@@ -25,10 +26,46 @@ void printHuffmanTree(TreeNode* root) {
     }
 }
 
-// Assign Huffman codes to each tree node
-// void assignBinaryCode(TreeNode* root) {
-//     if (root == '\0')
+// Recursive function to assign 0 and 1 to left and right nodes
+void dfs(TreeNode* node, char* code, int depth) {
 
-// }
+    if (node == NULL) return;
+
+    if (node->left == NULL && node->right == NULL) {
+        code[depth] = '\0';
+        printf("Character: %c, Code: %s\n", node->val, code);
+    }
+
+    // assign 0 to the left node
+    code[depth] = '0';
+    dfs(node->left, code, depth+1);
+
+    // assign 1 to the right node
+    code[depth] = '1';
+    dfs(node->right, code, depth+1);
+
+}
+
+// Get the height of the huffman tree
+int treeHieght(TreeNode* node) {
+
+    if (node == NULL) return 0;
+    
+    int leftHeight = treeHieght(node->left);
+    int rightHeight = treeHieght(node->right);
+
+
+    return (leftHeight >  rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+// Assign Huffman codes to each tree node
+void encode(TreeNode* root) {
+    int height = treeHieght(root);
+    char code[height]; 
+    dfs(root, code, 0);
+    
+}
+
+
 
 
